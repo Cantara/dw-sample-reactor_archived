b148d3302b8001076238e580c1b652fc007d397f
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2015-09-24 11:51:55 +0200

    Pull resource interface out to api

---
+++ dw-sample-api/src/main/java/no/cantara/dwsample/api/HelloWorldResource.java
@@ -0,0 +1,32 @@
+package no.cantara.dwsample.api;
+
+import com.google.common.base.Optional;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path(HelloWorldResource.PATH)
+@Api(HelloWorldResource.PATH)
+@Produces(MediaType.APPLICATION_JSON)
+public interface HelloWorldResource {
+
+    public static final String PATH = "hello-world";
+    public static final String QUERY_PARAM_GET_HELLO_NAME = "name";
+
+    @GET
+    @ApiOperation("Endpoint that will respond with hello and use the name provided as request parameter if any")
+    Saying hello(@QueryParam(QUERY_PARAM_GET_HELLO_NAME) @ApiParam(defaultValue = "Mr. Smith") Optional<String> name);
+
+    @POST
+    @ApiOperation("Post planetName and yourName and be greeted.")
+    @Consumes(MediaType.APPLICATION_JSON)
+    Saying hello(Planet planet);
+}
--- dw-sample-application/src/main/java/no/cantara/dwsample/resources/HelloWorldResource.java
+++ dw-sample-application/src/main/java/no/cantara/dwsample/resources/HelloWorldResource.java
@@ -2,9 +2,6 @@ package no.cantara.dwsample.resources;
 
 import com.codahale.metrics.annotation.Timed;
 import com.google.common.base.Optional;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import no.cantara.dwsample.api.Planet;
 import no.cantara.dwsample.api.Saying;
 import no.cantara.dwsample.domain.counter.CounterService;
@@ -12,20 +9,8 @@ import org.constretto.annotation.Configuration;
 import org.constretto.annotation.Configure;
 import org.springframework.stereotype.Service;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
-import java.util.concurrent.atomic.AtomicLong;
-
-@Path("/hello-world")
-@Api("/hello-world")
-@Produces(MediaType.APPLICATION_JSON)
 @Service
-public class HelloWorldResource {
+public class HelloWorldResource implements no.cantara.dwsample.api.HelloWorldResource {
     private final String template;
     private final String defaultName;
 
@@ -38,17 +23,12 @@ public class HelloWorldResource {
         this.counterService = counterService;
     }
 
-    @GET
-    @ApiOperation("Endpoint that will respond with hello and use the name provided as request parameter if any")
     @Timed
-    public Saying sayHello(@QueryParam("name") @ApiParam(defaultValue = "Mr. Smith") Optional<String> name) {
+    public Saying hello(Optional<String> name) {
         final String value = String.format(template, name.or(defaultName));
         return new Saying(counterService.next(), value);
     }
 
-    @POST
-    @ApiOperation("Post planetName and yourName and be greeted.")
-    @Consumes(MediaType.APPLICATION_JSON)
     @Timed
     public Saying hello(Planet planet) {
         final String value = "Hello " + planet.getYourName() + " on planet " + planet.getPlanetName();
